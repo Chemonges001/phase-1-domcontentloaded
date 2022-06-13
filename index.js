f17dfd16af9bd5e9f280c0507d5c785c365b49ef
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-13 18:12:06 +0300

    commit tree 7c00fa7c862dcaa1f1303a9f52851034ba8e07f5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  const text = document.getQuerySelector("#text")
-  id.textContent = "This is really cool!"
+  const text = document.querySelector("#text")
+  text.textContent = "This is really cool!"
 });
